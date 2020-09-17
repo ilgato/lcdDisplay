a24b956a7b1892834bbe5561d202b65a6198aa09
Pierangelo Castillo <pier.cm@gmail.com>
2020-09-17 02:56:07 +0200

    added images for the README
    made infinite loop for continuos input form user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,11 @@ public class Main {
         lcd.printLine("159");
         lcd.printLine("123e45678901234567");
         lcd.printLine("123456789012345678");
-        lcd.printLine(lcd.readUserInput());
-        lcd.setSymbols(new char[] {' ','|','-'});
-        lcd.setSymbolLineCombinations(lcd.getSymbols());
-        lcd.printLine("159");
+        while(true){
+            lcd.printLine(lcd.readUserInput());
+        }
+
+
     }
 
 
